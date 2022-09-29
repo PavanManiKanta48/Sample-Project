b95a5fb5f9f16cdd8af30ce86d0eb2d6fa30b0be
PavanManiKanta48 <113531282+PavanManiKanta48@users.noreply.github.com>
2022-09-29 22:46:34 +0530

    Add files via upload

---
+++ CSharpDotNetBasics/CSharpDotNetBasics/EmployeeClass/EmployeeClass/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeClass
+{
+    internal class Employee
+    {
+        private string  _FirstName;
+        
+        private string _LastName;
+
+        public string FirstName
+        {
+            get { return _FirstName; }      //The get Accessor is used to read the _EmployeeId private variable value
+            set { _FirstName = value; }     //The Set Accessor is used to set the _EmployeeId private variable value
+        }
+        public string SecondName
+        {
+            get { return _LastName; }
+            set { _LastName = value; }
+        }
+
+        public void FullName()        {
+            
+            Console.WriteLine(FirstName+" "+SecondName);
+        }
+    }
+}
---
+++ CSharpDotNetBasics/CSharpDotNetBasics/EmployeeClass/EmployeeClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee EmpObj = new Employee();
+            EmpObj.FirstName = "pavan";
+            EmpObj.SecondName = "manikanta";
+            EmpObj.FullName();
+        }
+    }
+}
